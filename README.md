a542ed67526b9f2549257bfdd882b35926ba0d14
Md.Sami Ul Haque <samiulh4@gmail.com>
2024-06-23 00:52:43 +0600

    npm chapter end

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # NodeJs Net Ninja Course
+
+npm install -g nodemon
+
+npm init
+
+npm install lodash@4.17.15
+
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
 
-    console.log('Request URL : ' + req.url);
-    console.log('Request Method : ' + req.method);
+    //console.log('Request URL : ' + req.url);
+    //console.log('Request Method : ' + req.method);
+
+    const num = _.random(0, 20);
+    console.log(num);
+
+    const greet = _.once(() => {
+        console.log('hello');
+      });
+      greet();
+      greet();
 
     // set header content type
     res.setHeader('Contect-Type', 'text/html');
@@ -17,13 +27,21 @@ const server = http.createServer((req, res) => {
     switch (req.url) {
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
             break;
         default:
             path += '404.html';
-            break;    
+            res.statusCode = 404;
+            break;
     }
 
     fs.readFile(path, (err, data) => {
